3ab0c023e4e3688e21572b909fd62bb1f153d50c
Yann Gallis <yann@gallis.me>
2022-02-25 17:30:41 +0100

    Add Fixture for admin

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,12 +5,29 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use App\Entity\User;
+
 class AppFixtures extends Fixture
 {
+    private $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $administrator = new User();
+
+        $administrator->setEmail("admin@test.com");
+        $administrator->setRoles(["ROLE_ADMIN"]);
+        $administrator->setPassword($this->hasher->hashPassword($administrator, 'password'));
+
+        $manager->persist($administrator);
+        $manager->flush();
+
 
         $manager->flush();
     }
