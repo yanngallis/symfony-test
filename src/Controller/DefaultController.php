a6419cc0e0e3bae1477a0c95a51e82f9e9e8c3bf
Yann Gallis <yann@gallis.me>
2022-02-25 17:42:47 +0100

    Add Products view on homepage

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,15 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Repository\ProductRepository;
+
 class DefaultController extends AbstractController
 {
     /**
      * @Route("/", name="app_default")
      */
-    public function index(): Response
+    public function index(ProductRepository $productRespository): Response
     {
+        $products = $productRespository->findAll();
+
         return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
+            'products' => $products,
         ]);
     }
 }
