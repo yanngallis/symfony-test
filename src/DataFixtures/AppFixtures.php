9c74c428417c20408e5d41771db1b3763e6ac8c8
Yann Gallis <yann@gallis.me>
2022-02-25 17:37:12 +0100

    Add Product fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -7,6 +7,7 @@ use Doctrine\Persistence\ObjectManager;
 
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use App\Entity\User;
+use App\Entity\Product;
 
 class AppFixtures extends Fixture
 {
@@ -20,14 +21,25 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $administrator = new User();
-
         $administrator->setEmail("admin@test.com");
         $administrator->setRoles(["ROLE_ADMIN"]);
         $administrator->setPassword($this->hasher->hashPassword($administrator, 'password'));
-
         $manager->persist($administrator);
-        $manager->flush();
 
+        $product1 = new Product();
+        $product1->setTitle("Product 1");
+        $product1->setPrice(10.00);
+        $manager->persist($product1);
+
+        $product2 = new Product();
+        $product2->setTitle("Product 2");
+        $product2->setPrice(12.99);
+        $manager->persist($product2);
+
+        $product3 = new Product();
+        $product3->setTitle("Product 3");
+        $product3->setPrice(49.99);
+        $manager->persist($product3);
 
         $manager->flush();
     }
